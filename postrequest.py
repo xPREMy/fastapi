036714ed04ef4c29e90b01775e7299ec82541e1f
xPREMy <b24bb1028@iitj.ac.in>
2025-07-30 05:47:50 +0530

    pydentic introduction for vaidation of posts for user

--- postrequest.py
+++ postrequest.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class post(BaseModel):
+    title : str
+    discription: str 
+    public : bool = True
+    rating : Optional[int] = None
 
 @app.get("/")
 def root():
     return {"message": "success"}
 
 @app.post("/post")
-def postin(var: dict = Body()):
+def postin(var : post):
     print(var)
-    return {"goodness": "good"}
+    return {"goodness": var}
